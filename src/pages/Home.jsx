b229275707a5bde2c8be282794fd3c6b6453a9c4
IndhuKaleeswaran <110532317+IndhuKaleeswaran@users.noreply.github.com>
2024-03-18 19:33:13 +0530

    Day1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Home;
